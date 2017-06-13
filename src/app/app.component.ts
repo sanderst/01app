2e779ee90fddf3ec31575cbc53916bb2a1157dc4
Travis <travsanders@gmail.com>
2017-06-13 15:33:27 -0600

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,12 @@ export class Hero {
   template: `
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
   `,
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heros';
