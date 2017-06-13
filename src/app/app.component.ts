8cf1ce2692987f55c4391cb4fc3e79f4e67c11b7
Travis <travsanders@gmail.com>
2017-06-13 15:24:37 -0600

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'Tour of Heros';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
