48887a2109d0b9ae73e7d79cf2bd983f5b35dad3
Travis <travsanders@gmail.com>
2017-06-18 06:46:59 -0600

    basic dash

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,9 +8,8 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
-  getHeroesSlowly(): Promise<Hero[]>{
-    return new Promise(resolve =>{
-      setTimeout(() => resolve(this.getHeroes()),2000);
-    });
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
